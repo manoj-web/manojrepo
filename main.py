1df8d6549bb8c542141dd9557b2ccf5c95cfca2e
manoj-web <77377472+manoj-web@users.noreply.github.com>
2023-01-19 08:43:31 +0530

    Added Initial Files

---
+++ main.py
@@ -0,0 +1,5 @@
+from ops import *
+
+red = add(5,4)
+
+print(red)
---
+++ test_opy.py
@@ -0,0 +1,9 @@
+from ops import *
+
+assert add(5,4) == 9, "Addition Failed"
+
+assert sub(5,4) == 1, "Subtraction Failed"
+
+assert mul(5,4) == 20, "Multiplication Failed"
+
+assert div(5,5) == 1, "Division Failed"
